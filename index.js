b2cdd93ab62f645218adde3587814e00b6ec46a7
unknown <jtredan@E6540-JTREDAN.Activeo.net>
2015-04-10 12:20:13 +0200

    Petis zanimals4

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.post('/slack', function(request, response) {
   var triggerWord = request.body.trigger_word; // the word that triggered this hook;
   
   
-  switch(triggerWord.toLowerString()){
+  switch(triggerWord.toLowerCase()){
 
 	case 'teub':
 	response.json({
