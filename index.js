e7f167b056a9aa9507d25b85f20024d1f7aece43
Arnaud Rinquin <rinquin.arnaud@gmail.com>
2015-04-10 09:49:53 +0100

    Add a slack-ready route
    On `POST /slack`. It just responds `Hello Slack`

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
+});
+
+app.post('/slack', function(request, response) {
+
+  // var messageText = request.body.text; // the original message text
+  // var triggerWord = request.body.trigger_word; // the word that triggered this hook;
+
+  response.json({
+    text: 'Hello Slack!'
+  });
 });
 
 app.listen(app.get('port'), function() {
